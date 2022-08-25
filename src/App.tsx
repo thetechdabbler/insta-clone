54261db8359a9cd72ceac5777a8b7cf7643c1a27
Manish Choudhary <choudharymanish@protonmail.com>
2022-08-25 02:14:24 +0000

    Added support for tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+            <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10" type="email" placeholder="jane@example.com" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className="text-red-500">
+          Edit <code>src/App.tsx</code> and save to reload code.
         </p>
         <a
           className="App-link"
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Here
         </a>
       </header>
     </div>
